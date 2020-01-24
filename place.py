55b6619c36c9d8ea3f4bdfe7d3d1fd34af5707b9
ItamarK5 <itamarkanne@gmail.com>
2020-01-24 17:32:29 +0200

    1.3.1
    completed register and completed url for r/place
    also changing some statifc files names

--- place.py
+++ place.py
@@ -56,8 +56,8 @@ def signup():
             
 @app.route('/place', methods=('GET',))
 @login_required
-def place(user_id):
-    pass
+def place():
+    return render_template('place.html')
 
 JOINED_PATH = path_join('web', 'static')
 @app.route('/files/<path:key>', methods=('GET',))
