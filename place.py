b051312215dad55e65f35cc12712d5b58fa78cd6
ItamarK5 <itamarkanne@gmail.com>
2020-01-24 13:10:28 +0200

    1.1
    added static file handler
    mimetypes dictionary to get each file of the static files

--- place.py
+++ place.py
@@ -6,12 +6,15 @@ from scripts.forms import *
 from os.path import join as path_join
 
 __all__ = ['app.py']
-app = Flask(__name__, static_folder='static', static_url_path='')
+app = Flask(__name__, static_folder='', static_url_path='')
 app = init_settings(app)
 app = init_alchemy(app)
 
 @app.route('/', methods=('GET', 'POST'),)
 def login():
+    """
+    added in version 1.0.0
+    """
     form = LoginForm()
     error_message = None
     if form.validate_on_submit():
@@ -25,6 +28,27 @@ def login():
     form.password.data = ''
     return render_template('forms/index.html', form=form, message=error_message)
 
-@app.route('/files/<string:key>')
+mimetypes = {
+    'png': 'image/png',
+    'ico': 'image/x-icon',
+    'css': 'text/css',
+    'js' : 'text/javascript'
+}
+
+@app.route('/files/<path:key>', methods=('GET',))
 def serve_static(key):
-    return send_from_directory(path_join('static', key.split('.')[-1]),key)
+    if key.rfind('.') == -1:
+        abort(405, 'unvalid file format')
+    # else
+    mimetype = mimetypes.get(key.split('.')[-1], None)
+    if mimetype is None:
+        abort(405, 'type not supported')
+    # return file
+    try:
+        return send_from_directory(
+            path_join('static', key.split(".")[-1]), key,
+            mimetype=mimetype
+        )
+    except Exception as e:
+        print('error', e)
+    abort(404, 'file dont found')
