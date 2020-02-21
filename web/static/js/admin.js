da2460f98a4f15210b3df2e22f59d443ef9ed355
ItamarK5 <itamarkanne@gmail.com>
2020-02-21 17:55:02 +0200

    2.9.7
    admin.js:
    --fixed error that included the head of the table
    edit.html:
    --current_page is in the admin domain and not profile
    profile.html:
    --added table close tag
    nav.html:
    --fixed error that dont color fa-tools but except if hovering it and not when setting it on specific page

--- web/static/js/admin.js
+++ web/static/js/admin.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
     $('[data-toggle="tooltip"]').tooltip();
     $('tr').click(function() {
-        window.location.href = `/edit/${$(this).children('.name-col').text()}`
+        let name = $(this).children('.name-col').text();
+        if(name){
+            window.location.href = `/edit/${$(this).children('.name-col').text()}`
+        };
     });
 });
