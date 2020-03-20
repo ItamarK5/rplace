66329fc83df3b62e442fec8d23ce65841a547947
ItamarK5 <itamarkanne@gmail.com>
2020-03-20 21:41:13 +0200

    Revert "Revert "3.5.1""
    This reverts commit 5e5a93eee364ed6f52f4ba70d7102e7c412c894b.

--- painter/backends/skio.py
+++ painter/backends/skio.py
@@ -134,16 +134,12 @@ def connect():
 @sio.on('change-lock-state', ADMIN_NAMESPACE)
 @socket_io_role_required(Role.admin)
 def change_lock_state(new_state: Any):
-    print(3, new_state)
     if not isinstance(new_state, bool):
         return {'success': False, 'response': 'Not A Valid Input'}
     # prevent collision
-    print(new_state)
     if lock.set_switch(new_state):
         sio.emit('change-lock-state', new_state, namespace=PAINT_NAMESPACE)
         sio.emit('set-lock-state', new_state, namespace=ADMIN_NAMESPACE, include_self=False)
         return {'success': True, 'response': new_state}
     else:
         return {'success': True, 'response': new_state}
-
-
