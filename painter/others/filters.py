7fabb9a9f76c56897fdeee1491aa51746024b6c7
ItamarK5 <itamarkanne@gmail.com>
2020-04-08 00:12:05 +0300

    4.13.7
    nicer names

--- painter/others/filters.py
+++ painter/others/filters.py
@@ -5,6 +5,7 @@ from flask import Flask
 from painter.models.user import User, Role
 from painter.others.constants import COLORS
 from painter.others.constants import MINUTES_COOLDOWN
+from datetime import datetime
 
 
 def draw_time(user: User) -> str:
@@ -13,7 +14,7 @@ def draw_time(user: User) -> str:
         return 'never'
     # else
     return (user.next_time - MINUTES_COOLDOWN).strftime(
-        '%y-%m-%d %a %H:%M:%S.%f'
+        '%y-%m-%d %a %H:%M:%S'
     )
 
 
@@ -40,8 +41,13 @@ def color(color_idx: int) -> str:
     return COLORS[color_idx]
 
 
+def date(tm: datetime) -> str:
+    return tm.strftime('%a, %x')
+
+
 def add_filters(flask_app: Flask) -> None:
     flask_app.add_template_filter(draw_time, 'draw_time')
     flask_app.add_template_filter(is_admin, 'is_admin')
     flask_app.add_template_filter(class_ftr, 'class_ftr')
     flask_app.add_template_filter(color, 'color')
+    flask_app.add_template_filter(date, 'date')
