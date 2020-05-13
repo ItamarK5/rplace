a7fa84289daeadf3d830859abb26a5229c727979
ItamarK5 <itamarkanne@gmail.com>
2020-05-13 20:29:31 +0300

    6.2(ext)
    graph

--- painter/manager.py
+++ painter/manager.py
@@ -269,3 +269,15 @@ def clear_cache():
 
 # adds clear cache command
 manager.add_command('clear-cache', Command(clear_cache))
+@manager.command
+def graph():
+    from sqlalchemy_schemadisplay import create_schema_graph
+
+    # create the pydot graph object by autoloading all tables via a bound metadata object
+    graph = create_schema_graph(metadata=storage_sql.metadata,
+                                show_datatypes=True,  # The image would get nasty big if we'd show the datatypes
+                                show_indexes=False,  # ditto for indexes
+                                rankdir='LR',  # From left to right (instead of top to bottom)
+                                concentrate=False  # Don't try to join the relation lines together
+                                )
+    graph.write_png('dbschema.png')  # write out the file
